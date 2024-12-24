bf1104125892974a5424a5f4faf1dacfc10a68e8
Kauanrodrigues01m <kauanrl09@gmail.com>
2024-12-23 23:42:52 -0300

    update

--- outflows/signals.py
+++ outflows/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from .models import Outflow
-
+from decouple import config
+from django.contrib.auth import get_user_model
 
 @receiver(post_save, sender=Outflow)
 def update_product_quantity(sender, instance, created, **kwargs):
@@ -10,3 +11,26 @@ def update_product_quantity(sender, instance, created, **kwargs):
             product = instance.product
             product.quantity -= instance.quantity
             product.save()
+
+
+@receiver(post_migrate)
+def create_user(sender, **kwargs):
+    User = get_user_model()
+
+    # Verifica se o usuário já existe no banco de dados. Se existir, não cria outro.
+    if User.objects.exists():
+        return
+
+    # Se não existir, cria um novo usuário com as variáveis de ambiente
+    username = config('USERNAME_TEST', cast=str)
+    email = config('EMAIL', cast=str)
+    password = config('PASSWORD', cast=str)
+    
+    # Criando o usuário com as informações do .env
+    t = User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+    
+    print(f'Criado o usuário: {t}')
